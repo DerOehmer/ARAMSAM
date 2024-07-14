e1fcffbcbb0d5c3e4932c104bd82af8dce00aa24
Jonas Boysen <jonas.boysen@googlemail.com>
2024-07-14 16:41:04 +0000

    rework ui to enable multi scroll and zoom + coord tracking

--- sam_annotator/gui.py
+++ sam_annotator/gui.py
@@ -1,7 +1,17 @@
 import numpy as np
-import PyQt6 as Qt
 
-from PyQt6.QtGui import QPixmap, QImage, QKeyEvent
+from PyQt6.QtCore import Qt, QPoint, QRectF, pyqtSignal
+from PyQt6.QtGui import (
+    QPixmap,
+    QImage,
+    QKeyEvent,
+    QWheelEvent,
+    QMouseEvent,
+    QBrush,
+    QColor,
+    QCursor,
+    QTransform,
+)
 from PyQt6.QtWidgets import (
     QWidget,
     QMainWindow,
@@ -10,6 +20,10 @@ from PyQt6.QtWidgets import (
     QPushButton,
     QTabWidget,
     QMessageBox,
+    QGraphicsView,
+    QGraphicsScene,
+    QGraphicsPixmapItem,
+    QFrame,
 )
 
 
@@ -20,9 +34,12 @@ class UserInterface(QMainWindow):
         self.top = 10
         img_shape = (512, 512)
         offset = 50
-        self.resize(img_shape[0] * 3 + offset, img_shape[1] + offset)
+        # self.resize(img_shape[0] * 2 + offset, img_shape[1] * 2 + offset)
+        self.resize(1920, 1080)
         self.menu = self.menuBar().addMenu("&Menu")
         self.menu.addAction("&Exit", self.close)
+        self.zoom_level = 0
+        self.zoom_factor = 1.25
 
         self.construct_ui(img_shape=img_shape, offset=offset)
 
@@ -31,27 +48,42 @@ class UserInterface(QMainWindow):
         img = np.zeros((img_shape[0], img_shape[1], 3), dtype="uint8")
         self.img = QImage(img, img_shape[0], img_shape[1], QImage.Format.Format_RGB888)
         self.viz_labels: list[QLabel] = []
-        self.create_pixmaps(img_shape=img_shape, offset=offset)
 
-        # self.tabs = QTabWidget(parent=self)
-        # self.sequence_tab = QWidget()
-        # self.img_label = QLabel(self)
-        # self.img_label.move(offset, offset)
-        # self.img_label.resize(img_shape[0], img_shape[1])
-        # self.img_label.setPixmap(QPixmap.fromImage(self.img))
-        # self.img_label.show()
+        self.labelCoords = QLabel(self, text="adasdasd")
+        self.labelCoords.move(400, 20)
+        self.labelCoords.show()
 
-        # self.mask_label = QLabel(self)
-        # self.mask_label.move(offset + img_shape[0], offset)
-        # self.mask_label.resize(img_shape[0], img_shape[1])
-        # self.mask_label.setPixmap(QPixmap.fromImage(self.img))
-        # self.mask_label.show()
+        vis_width = int((1920 - (offset * 16 / 9)) / 2)
+        vis_height = int((1080 - offset) / 2)
 
-        # self.masked_img_label = QLabel(self)
-        # self.masked_img_label.move(offset + 2 * img_shape[0], offset)
-        # self.masked_img_label.resize(img_shape[0], img_shape[1])
-        # self.masked_img_label.setPixmap(QPixmap.fromImage(self.img))
-        # self.masked_img_label.show()
+        self.annotation_visualizers: list[InteractiveGraphicsView] = []
+        for i in range(2):
+            for j in range(2):
+                annotation_visualizer = InteractiveGraphicsView(
+                    self, width=vis_width, height=vis_height
+                )
+                annotation_visualizer.move(
+                    int(i * vis_width / 2), int(j * vis_height / 2)
+                )
+                annotation_visualizer.setGeometry(
+                    offset + i * vis_width,
+                    offset + j * vis_height,
+                    vis_width,
+                    vis_height,
+                )
+                annotation_visualizer.show()
+                annotation_visualizer.coordinatesChanged.connect(self.handleCoords)
+                annotation_visualizer.mouseWheelScroll.connect(self.wheelEvent)
+                annotation_visualizer.mouseMove.connect(self.childMoveEvent)
+                self.annotation_visualizers.append(annotation_visualizer)
+
+        # for idx, ann_vis in enumerate(self.annotation_visualizers):
+        #     for i in range(4):
+        #         if idx == i:
+        #             continue
+        #         ann_vis.scaled.connect(self.annotation_visualizers[idx].set_transform)
+
+        # self.create_pixmaps(img_shape=img_shape, offset=offset)
 
         self.test_button = QPushButton(text="segment anything!", parent=self)
         self.test_button.move(20, 20)
@@ -61,6 +93,12 @@ class UserInterface(QMainWindow):
 
         self.bad_mask_button = QPushButton(text="bad mask", parent=self)
         self.bad_mask_button.move(220, 20)
+
+    def handleCoords(self, point: QPoint):
+        if not point.isNull():
+            self.labelCoords.setText(f"{point.x()}, {point.y()}")
+        else:
+            self.labelCoords.clear()
 
     def create_pixmaps(self, img_shape: tuple[int], offset: int):
         for i in range(3):
@@ -79,6 +117,74 @@ class UserInterface(QMainWindow):
             self.good_mask_button.click()
         elif event.text() == "m":
             self.bad_mask_button.click()
+
+    def childMoveEvent(self, event: QMouseEvent):
+        scene_idx_under_mouse = self.get_annotation_visualizer_idx_under_mouse()
+        if scene_idx_under_mouse is None:
+            print("not on image")
+            return
+
+        viewport = (
+            self.annotation_visualizers[scene_idx_under_mouse]
+            .mapToScene(
+                self.annotation_visualizers[scene_idx_under_mouse].viewport().geometry()
+            )
+            .boundingRect()
+        )
+        for idx, ann_viz in enumerate(self.annotation_visualizers):
+            if idx == scene_idx_under_mouse:
+                continue
+            ann_viz.setSceneRect(viewport)
+
+    def wheelEvent(self, event: QWheelEvent):
+        scene_idx_under_mouse = self.get_annotation_visualizer_idx_under_mouse()
+        if scene_idx_under_mouse is None:
+            print("Zooming only on images")
+            return
+
+        if event.angleDelta().y() < 0:
+            if self.zoom_level == 0:
+                print("cant zoom further out")
+                return
+            elif self.zoom_level == 1:
+                self.zoom_level = 0
+                self.fit_annotation_visualizers_to_view()
+                return
+            else:
+                self.zoom_level -= 1
+                factor = 1 / self.zoom_factor
+
+        if event.angleDelta().y() > 0:
+            self.zoom_level += 1
+            factor = self.zoom_factor
+
+        # scale and save viewport and transformation of scene under mouse
+        self.annotation_visualizers[scene_idx_under_mouse].scale(factor, factor)
+        viewport = (
+            self.annotation_visualizers[scene_idx_under_mouse]
+            .mapToScene(
+                self.annotation_visualizers[scene_idx_under_mouse].viewport().geometry()
+            )
+            .boundingRect()
+        )
+        # apply to others
+        for idx, ann_viz in enumerate(self.annotation_visualizers):
+            if idx == scene_idx_under_mouse:
+                continue
+            ann_viz.scale(factor, factor)
+            ann_viz.setSceneRect(viewport)
+
+    def get_annotation_visualizer_idx_under_mouse(self) -> int:
+        scene_idx_under_mouse = None
+
+        for idx, ann_viz in enumerate(self.annotation_visualizers):
+            if ann_viz.underMouse():
+                scene_idx_under_mouse = idx
+        return scene_idx_under_mouse
+
+    def fit_annotation_visualizers_to_view(self):
+        for ann_viz in self.annotation_visualizers:
+            ann_viz.fitInView()
 
     def create_message_box(self, crticial: bool = False, text: str = ""):
         self.msg_box = QMessageBox(self)
@@ -112,4 +218,90 @@ class UserInterface(QMainWindow):
 
     def update_main_pix_map(self, idx: int, img: np.ndarray):
         q_img = self.convert_ndarray_to_qimage(img=img)
-        self.viz_labels[idx].setPixmap(QPixmap.fromImage(q_img))
+        pixmap = QPixmap.fromImage(q_img)
+        # self.viz_labels[idx].setPixmap(QPixmap.fromImage(q_img))
+        self.annotation_visualizers[idx].set_pixmap(pixmap=pixmap)
+
+
+class InteractiveGraphicsView(QGraphicsView):
+    coordinatesChanged: pyqtSignal = pyqtSignal(QPoint)
+    mouseWheelScroll: pyqtSignal = pyqtSignal(QWheelEvent)
+    mouseMove: pyqtSignal = pyqtSignal(QMouseEvent)
+    clicked: pyqtSignal = pyqtSignal(bool)
+
+    def __init__(self, parent, width: int = 1024, height: int = 1024) -> None:
+        super().__init__(parent=parent)
+
+        self.width = width
+        self.height = height
+        self.zoom_val = 0
+        self.mouse_down = False
+        self.graphics_scene = QGraphicsScene(self)
+        self.pixmap_item = QGraphicsPixmapItem()
+        self.pixmap_item.setShapeMode(QGraphicsPixmapItem.ShapeMode.BoundingRectShape)
+
+        self.graphics_scene.addItem(self.pixmap_item)
+        self.setScene(self.graphics_scene)
+
+        self.setTransformationAnchor(QGraphicsView.ViewportAnchor.AnchorUnderMouse)
+        self.setResizeAnchor(QGraphicsView.ViewportAnchor.AnchorUnderMouse)
+        self.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
+        self.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
+        self.setBackgroundBrush(QBrush(QColor(30, 30, 30)))
+        self.setFrameShape(QFrame.Shape.NoFrame)
+
+    def set_pixmap(self, pixmap: QPixmap):
+        self.setDragMode(QGraphicsView.DragMode.ScrollHandDrag)
+        self.pixmap_item.setPixmap(pixmap)
+        self.fitInView()
+
+    def wheelEvent(self, event: QWheelEvent):
+        # and overwrite superclass method to avoid scrolling on mouse wheel
+        self.mouseWheelScroll.emit(event)
+
+    def resizeEvent(self, event):
+        super().resizeEvent(event)
+        self.fitInView()
+
+    def leaveEvent(self, event):
+        self.coordinatesChanged.emit(QPoint())
+        super().leaveEvent(event)
+
+    def mouseMoveEvent(self, event: QMouseEvent):
+        self.updateCoordinates(event.position().toPoint())
+        if self.mouse_down:
+            self.mouseMove.emit(event)
+        super().mouseMoveEvent(event)
+
+    def mousePressEvent(self, event: QMouseEvent):
+        self.mouse_down = True
+        super().mousePressEvent(event)
+
+    def mouseReleaseEvent(self, event: QMouseEvent):
+        self.mouse_down = False
+        super().mouseReleaseEvent(event)
+
+    def updateCoordinates(self, pos=None):
+        if self.pixmap_item.isUnderMouse():
+            if pos is None:
+                pos = self.mapFromGlobal(QCursor.pos())
+            point = self.mapToScene(pos).toPoint()
+        else:
+            point = QPoint()
+        self.coordinatesChanged.emit(point)
+
+    def fitInView(self):  # , scale=True):
+        rect = QRectF(self.pixmap_item.pixmap().rect())
+        if not rect.isNull():
+            self.setSceneRect(rect)
+            # if self.hasPhoto():
+            unity = self.transform().mapRect(QRectF(0, 0, 1, 1))
+            self.scale(1 / unity.width(), 1 / unity.height())
+            viewrect = self.viewport().rect()
+            scenerect = self.transform().mapRect(rect)
+            factor = min(
+                viewrect.width() / scenerect.width(),
+                viewrect.height() / scenerect.height(),
+            )
+            self.scale(factor, factor)
+            self.zoom_val = 0
