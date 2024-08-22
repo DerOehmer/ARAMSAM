e4e152eea35752e7026d313ae74f349414a66e5e
DerOehmer <leon.oehme@uni-hohenheim.de>
2024-08-22 16:23:24 +0200

    Adding Sam2 class. The structure follows the SAM class. Custom AMG functionality is assigned a own class to be used by both SAM versions

--- src/run_sam.py
+++ src/run_sam.py
@@ -30,6 +30,7 @@ class SamInference:
         self.sam.to(device=self.device)
 
         self.predictor = CustomSamPredictor(self.sam)
+        self.custom_amg = CustomAMG(self)
         self.img = None
 
     def amg(self, img, msize_thresh=10000, pt_grid=None):
@@ -71,107 +72,6 @@ class SamInference:
         masks = np.array(masks, dtype=np.uint8)[order]
         return masks, annotated_image
 
-    def custom_amg(self, roi_pts=False, n_points=100, msize_thresh=10000):
-        pts = None
-        pt_labels = None
-        if roi_pts:
-            pts, pt_labels = self.get_roi_points(n_points)
-        else:
-            pts, pt_labels = self.get_pt_grid(n_points)
-        transformed_pts = self.predictor.transform.apply_coords_torch(
-            pts, self.img.shape[:2]
-        )
-        masks = self.predict_batch(transformed_pts, pt_labels)
-
-        xmins = []
-        ymins = []
-        mask_lst = []
-        mask_objs = []
-
-        annotated_image = self.imgbgr.copy()
-
-        for mask in masks:
-            mask = mask.cpu().numpy()
-            if np.count_nonzero(mask) < msize_thresh:
-                kernelmask = np.array(mask, dtype=np.uint8) * 255
-                kernelmask = np.squeeze(kernelmask, axis=0)
-                if np.amax(kernelmask) != 255:
-                    continue
-                mask_lst.append(kernelmask)
-                mask_objs.append(MaskData(kernelmask, "sam_proposed"))
-
-                ycords, xcords = np.where(kernelmask == 255)
-                # annotated_image[ycords, xcords] = b, g, r
-                ymin, xmin = np.amin(ycords), np.amin(xcords)
-
-                ymins.append(ymin)
-                xmins.append(xmin)
-        mvis = MaskVisualization(img=annotated_image, mask_objs=mask_objs)
-        annotated_image = mvis.get_masked_img()
-        mvis_mask_objs = mvis.mask_objs
-
-        cord_dict = {"ymins": ymins, "xmins": xmins}
-        cord_df = pd.DataFrame(cord_dict)
-        cord_df = cord_df.sort_values(by=["ymins", "xmins"])
-        order = cord_df.index.to_list()
-        # masks_np = np.array(mask_lst, dtype=np.uint8)[order]
-        ordered_mask_objs = [mvis_mask_objs[i] for i in order]
-        return ordered_mask_objs, annotated_image
-
-    def get_roi_points(self, n=100, lower_hsv=[28, 0, 0], higher_hsv=[179, 255, 255]):
-        lower_hsv = np.array(lower_hsv, dtype=np.uint8)
-        higher_hsv = np.array(higher_hsv, dtype=np.uint8)
-        imgBLUR = cv2.GaussianBlur(self.imgbgr, (3, 3), 0)
-        imgHSV = cv2.cvtColor(imgBLUR, cv2.COLOR_BGR2HSV)
-        mask = cv2.inRange(imgHSV, lower_hsv, higher_hsv)
-        cv2.imshow("mask", mask)
-        cv2.waitKey(0)
-        indcs = np.column_stack(np.where(mask == 255))
-        if len(indcs) < n:
-            n = len(indcs)
-        rand_indcs = indcs[np.random.choice(indcs.shape[0], n, replace=False)]
-        roi_points = rand_indcs[:, [1, 0]]
-        pt_tensor = torch.tensor(
-            roi_points.reshape(-1, 1, 2), dtype=torch.int32, device=self.device
-        )
-        label_tensor = torch.ones((n, 1), dtype=torch.int32, device=self.device)
-
-        return pt_tensor, label_tensor
-
-    def get_pt_grid(self, n):
-        height, width, _ = self.img.shape
-        # Total number of points
-
-        # Calculate the number of rows and columns for the grid
-        grid_size = int(np.sqrt(n))
-        rows, cols = grid_size, grid_size
-
-        pt_distx = width / cols
-        pad_x = int(pt_distx / 2)
-        pt_disty = height / rows
-        pad_y = int(pt_disty / 2)
-        # Generate coordinates for grid points
-        row_indices = np.linspace(pad_y, height - 1 - pad_y, rows, dtype=int)
-        col_indices = np.linspace(pad_x, width - 1 - pad_x, cols, dtype=int)
-
-        # Create a meshgrid of row and column indices
-        grid_y, grid_x = np.meshgrid(row_indices, col_indices)
-
-        # Flatten the meshgrid to get a list of coordinates
-        grid_points = np.vstack([grid_y.ravel(), grid_x.ravel()]).T
-
-        pt_tensor = torch.tensor(
-            grid_points.reshape(-1, 1, 2), dtype=torch.int32, device=self.device
-        )
-        label_tensor = torch.ones(
-            (rows * cols, 1), dtype=torch.int32, device=self.device
-        )
-        return pt_tensor, label_tensor
-
-    def image_embedding(self, img):
-        self.imgbgr = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-        self.img = img
-
     def predict_batch(
         self,
         pts: torch.Tensor = None,
@@ -207,54 +107,16 @@ class SamInference:
             mask_input=mask_input,
             multimask_output=False,
         )
-        return masks[0].astype(np.uint8)
-
-    def select_masks(self, masks, alpha=0.1):
-        while True:
-            for mask in masks:
-                mask = mask.astype(np.uint8)
-                green_mask = cv2.merge(
-                    [
-                        np.zeros_like(mask),
-                        255 * (mask > 0).astype(np.uint8),
-                        np.zeros_like(mask),
-                    ]
-                )
-                result0 = cv2.addWeighted(self.imgbgr, 1 - alpha, green_mask, alpha, 0)
-                result = self.zoom_in(result0, mask)
-                cv2.imshow("mask", result)
-                key = cv2.waitKey(0) & 0xFF
-                if key == ord("n"):
-                    cv2.destroyWindow("mask")
-                    return mask * 255
-                if key == ord("m"):
-                    cv2.destroyWindow("mask")
-                    return []
-                if key == ord("x"):
-                    cv2.destroyWindow("mask")
-                    raise KeyboardInterrupt()
-
-    def zoom_in(self, annotation, binmask, zoom_factor=3):
-        yindcs, xindcs = np.where(binmask > 0)
-        ymin, ymax = np.amin(yindcs), np.amax(yindcs)
-        xmin, xmax = np.amin(xindcs), np.amax(xindcs)
-        w, h = xmax - xmin, ymax - ymin
-        zoom_sizex, zoom_sizey = int(zoom_factor * w), int(
-            zoom_factor * h
-        )  # Adjust zoom size as needed
-        zoomxy = max(0, xmin - zoom_sizex), max(0, ymin - zoom_sizey)
-        x1, y1 = zoomxy
-        x2, y2 = min(annotation.shape[1], xmax + zoom_sizex), min(
-            annotation.shape[0], ymax + zoom_sizey
-        )
-
-        # Extract zoomed region and resize
-        zoomed_img0 = annotation[y1:y2, x1:x2]
-        zoomed_img = cv2.resize(zoomed_img0, (0, 0), fx=zoom_factor, fy=zoom_factor)
-        return zoomed_img
+        return masks[0].astype(np.uint8) * 255
 
     def reset_img(self):
         self.predictor.reset_image()
+
+    def _prep_batch_pts_and_lbls(self, pts, lbls, shp: tuple[int, int]):
+        pt_tensor = torch.tensor(pts, dtype=torch.int32, device=self.device)
+        label_tensor = torch.tensor(lbls, dtype=torch.int32, device=self.device)
+        transformed_pts = self.predictor.transform.apply_coords_torch(pt_tensor, shp)
+        return transformed_pts, label_tensor
 
 
 class CustomSamPredictor(SamPredictor):
@@ -312,3 +174,237 @@ class CustomSamPredictor(SamPredictor):
         self.input_size = input_size
         self.features = features
         self.is_image_set = True
+
+
+class Sam2Inference:
+    def __init__(
+        self,
+        sam2_checkpoint: str = "sam2_hiera_small.pt",
+        cfg_path: str = "sam2_hiera_s.yaml",
+    ):
+        from sam2.build_sam import build_sam2_video_predictor
+        from sam2.sam2_video_predictor import SAM2VideoPredictor
+
+        if not torch.cuda.is_available():
+            raise RuntimeError(
+                "CUDA is not available. SAM2 requires a CUDA enabled GPU."
+            )
+
+        self.device = "cuda"
+
+        torch.autocast(device_type="cuda", dtype=torch.bfloat16).__enter__()
+
+        if torch.cuda.get_device_properties(0).major >= 8:
+            # turn on tfloat32 for Ampere GPUs (https://pytorch.org/docs/stable/notes/cuda.html#tensorfloat-32-tf32-on-ampere-devices)
+            torch.backends.cuda.matmul.allow_tf32 = True
+            torch.backends.cudnn.allow_tf32 = True
+
+        self.predictor: SAM2VideoPredictor = build_sam2_video_predictor(
+            cfg_path, sam2_checkpoint, device="cuda"
+        )
+        self.custom_amg = CustomAMG(self)
+        self.imgs: list[np.ndarray] = None  # expecting len 2 rgb uint8 images
+        self.inference_state: dict = None
+        self.latest_obj_id = 0
+        self.predictor.is_image_set = False
+
+    def set_features(self, imgs: list[np.ndarray]):
+        if self.predictor.is_image_set:
+            self.predictor.reset_state(self.inference_state)
+        self.imgs = imgs
+        assert (
+            len(imgs) == 2 and imgs[0].dtype == np.uint8 and imgs[0].shape[2] == 3
+        ), "Expecting exactly 2 RGB Numpy arrays with shape (H, W, 3) and dtype uint8"
+
+        self.inference_state = self.predictor.init_state(
+            video_path=imgs,
+            offload_video_to_cpu=False,
+            offload_state_to_cpu=False,
+            async_loading_frames=True,
+        )
+        self.predictor.is_image_set = True
+
+    def predict(self, pts: np.ndarray, pts_labels: np.ndarray):
+        # single object
+        assert (
+            pts.dtype == np.float32 and pts.shape[-1] == 2
+        ), "Expecting numpy array with shape (N, 2) and dtype float32"
+        assert (
+            pts_labels.dtype == np.int32 and pts_labels.shape[-1] == 1
+        ), "Expecting numpy array with shape (N, 1) and dtype int32"
+
+        _, out_obj_ids, out_mask_logits = self.predictor.add_new_points(
+            inference_state=self.inference_state,
+            frame_idx=0,
+            obj_id=0,
+            points=pts,
+            labels=pts_labels,
+        )
+
+        self.predictor.reset_state(self.inference_state)
+        return self._logits_to_npmask(out_mask_logits[-1])
+
+    def predict_batch(self, pts: np.ndarray, pts_labels: np.ndarray):
+        # multiple objects
+        assert (
+            pts.dtype == np.float32 and pts.shape[-1] == 2 and pts.ndim == 3
+        ), "Expecting numpy array with shape (N, M, 2) and dtype float32"
+        assert (
+            pts_labels.dtype == np.int32
+            and pts_labels.shape[-1] == 1
+            and pts_labels.ndim == 2
+        ), "Expecting numpy array with shape (N, M, 1) and dtype int32"
+        masks = []
+        for obj_idx in range(pts.shape[0]):
+            _, out_obj_ids, out_mask_logits = self.predictor.add_new_points(
+                inference_state=self.inference_state,
+                frame_idx=0,
+                obj_id=self.latest_obj_id,
+                points=pts[obj_idx],
+                labels=pts_labels[obj_idx],
+            )
+            self.latest_obj_id += 1
+            masks.append(out_mask_logits[obj_idx] > 0.0)
+        return masks
+
+    def set_masks(self, mask_objs: list[MaskData]):
+        self.predictor.reset_state(self.inference_state)
+        for m in mask_objs:
+            assert (
+                m.mask.dtype == np.uint8 and m.mask.ndim == 2
+            ), "Expecting numpy array with 2 dims and dtype bool"
+            mask = m.mask.astype(bool)
+            self.predictor.add_new_mask(
+                self.inference_state, 0, self.latest_obj_id, mask
+            )
+            self.latest_obj_id += 1
+
+    def propagate_to_next_img(self):
+        prop_masks: list[MaskData] = []
+        for (
+            out_frame_idx,
+            out_obj_ids,
+            out_mask_logits,
+        ) in self.predictor.propagate_in_video(self.inference_state):
+            if out_frame_idx == 1:
+                for i, out_obj_id in enumerate(out_obj_ids):
+                    np_mask = self._logits_to_npmask(out_mask_logits[i])
+                    if np_mask is not None:
+                        prop_masks.append(MaskData(np_mask, "sam2_propagated"))
+        return prop_masks
+
+    def _logits_to_npmask(self, out_mask_logit):
+        boolmask = (out_mask_logit > 0.0).cpu().numpy()
+        if np.any(boolmask):
+            npmask = np.transpose(boolmask, (1, 2, 0))
+            npmask = np.array(npmask, dtype=np.uint8).reshape(npmask.shape[:2]) * 255
+            return npmask
+        else:
+            return None
+
+    def _prep_batch_pts_and_lbls(
+        self, pts: np.ndarray, lbls: np.ndarray, shp: tuple[int, int]
+    ):
+        return pts.astype(np.float32), lbls
+
+
+class CustomAMG:
+    def __init__(self, sam_cls: SamInference | Sam2Inference) -> None:
+        self.sam_cls: SamInference | Sam2Inference = sam_cls
+
+    def __call__(self, roi_pts=False, n_points=100, msize_thresh=10000):
+        pts = None
+        pt_labels = None
+        if roi_pts:
+            pts, pt_labels = self._get_roi_points(n_points)
+        else:
+            pts, pt_labels = self._get_pt_grid(n_points)
+        transformed_pts, transformed_lbls = self.sam_cls._prep_batch_pts_and_lbls(
+            pts, pt_labels, self.imgbgr.shape[:2]
+        )
+        masks = self.sam_cls.predict_batch(transformed_pts, transformed_lbls)
+
+        xmins = []
+        ymins = []
+        mask_lst = []
+        mask_objs = []
+
+        annotated_image = self.imgbgr.copy()
+
+        for mask in masks:
+            mask = mask.cpu().numpy()
+            if np.count_nonzero(mask) < msize_thresh and np.any(mask):
+                kernelmask = np.array(mask, dtype=np.uint8) * 255
+                kernelmask = np.squeeze(kernelmask, axis=0)
+                if np.amax(kernelmask) != 255:
+                    continue
+                mask_lst.append(kernelmask)
+                mask_objs.append(MaskData(kernelmask, "sam_proposed"))
+
+                ycords, xcords = np.where(kernelmask == 255)
+                # annotated_image[ycords, xcords] = b, g, r
+                ymin, xmin = np.amin(ycords), np.amin(xcords)
+
+                ymins.append(ymin)
+                xmins.append(xmin)
+        mvis = MaskVisualization(img=annotated_image, mask_objs=mask_objs)
+        annotated_image = mvis.get_masked_img()
+        mvis_mask_objs = mvis.mask_objs
+
+        cord_dict = {"ymins": ymins, "xmins": xmins}
+        cord_df = pd.DataFrame(cord_dict)
+        cord_df = cord_df.sort_values(by=["ymins", "xmins"])
+        order = cord_df.index.to_list()
+        # masks_np = np.array(mask_lst, dtype=np.uint8)[order]
+        ordered_mask_objs = [mvis_mask_objs[i] for i in order]
+        return ordered_mask_objs, annotated_image
+
+    def _get_roi_points(self, n=100, lower_hsv=[28, 0, 0], higher_hsv=[179, 255, 255]):
+        lower_hsv = np.array(lower_hsv, dtype=np.uint8)
+        higher_hsv = np.array(higher_hsv, dtype=np.uint8)
+        imgBLUR = cv2.GaussianBlur(self.imgbgr, (3, 3), 0)
+        imgHSV = cv2.cvtColor(imgBLUR, cv2.COLOR_BGR2HSV)
+        mask = cv2.inRange(imgHSV, lower_hsv, higher_hsv)
+        cv2.imshow("mask", mask)
+        cv2.waitKey(0)
+        indcs = np.column_stack(np.where(mask == 255))
+        if len(indcs) < n:
+            n = len(indcs)
+        rand_indcs = indcs[np.random.choice(indcs.shape[0], n, replace=False)]
+        roi_points = rand_indcs[:, [1, 0]]
+        pt_tensor = torch.tensor(
+            roi_points.reshape(-1, 1, 2), dtype=torch.int32, device=self.sam_cls.device
+        )
+        label_tensor = torch.ones((n, 1), dtype=torch.int32, device=self.sam_cls.device)
+
+        return pt_tensor, label_tensor
+
+    def _get_pt_grid(self, n):
+        height, width, _ = self.imgbgr.shape
+        # Total number of points
+
+        # Calculate the number of rows and columns for the grid
+        grid_size = int(np.sqrt(n))
+        rows, cols = grid_size, grid_size
+
+        pt_distx = width / cols
+        pad_x = int(pt_distx / 2)
+        pt_disty = height / rows
+        pad_y = int(pt_disty / 2)
+        # Generate coordinates for grid points
+        row_indices = np.linspace(pad_y, height - 1 - pad_y, rows, dtype=int)
+        col_indices = np.linspace(pad_x, width - 1 - pad_x, cols, dtype=int)
+
+        # Create a meshgrid of row and column indices
+        grid_y, grid_x = np.meshgrid(row_indices, col_indices)
+
+        # Flatten the meshgrid to get a list of coordinates
+        grid_points = np.vstack([grid_y.ravel(), grid_x.ravel()]).T
+
+        pts_np = grid_points.reshape(-1, 1, 2)
+        lbls_np = np.ones((rows * cols, 1), dtype=np.int32)
+
+        return pts_np, lbls_np
+
+    def set_visualization_img(self, img):
+        self.imgbgr = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
