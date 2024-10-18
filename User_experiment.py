521c03918cff437a4ed2fa95d5c1cf4d45772bf5
DerOehmer <leon.oehme@uni-hohenheim.de>
2024-10-18 15:28:34 +0200

    Add natsorted to User experiment

--- User_experiment.py
+++ User_experiment.py
@@ -1,4 +1,5 @@
 from sam_annotator.main import create_vis_options
+from natsort import natsorted
 from sam_annotator.app import App
 from PyQt6.QtTest import QTest
 import sys
@@ -25,7 +26,7 @@ def mock_main():
     }
     app = App(ui_options=ui_options, experiment_mode="structured")
     app.output_dir = "/home/geink81/pythonstuff/SequenceSAM-Annotator/output"
-    app.sam_gen = 2
+    app.sam_gen = 1
     ui = app.ui
     ui.open_load_folder_dialog = mock_open_load_folder_dialog
     ui.run()
@@ -46,7 +47,7 @@ def mock_main():
 
 if __name__ == "__main__":
     root_p = "/home/geink81/pythonstuff/SequenceSAM-Annotator/EarImgPairs"
-    for img_pair_folder_p in glob.glob(f"{root_p}/*"):
+    for img_pair_folder_p in natsorted(glob.glob(f"{root_p}/*")):
         update_mock_path(img_pair_folder_p)
         mock_main()
     sys.exit()
