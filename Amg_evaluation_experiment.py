37c32a3c70b1c9a18b7e776ea069391c236f179f
Jonas Boysen <jonas.boysen@googlemail.com>
2024-10-24 14:24:17 +0000

    final prototype for amg_experiment

--- Amg_evaluation_experiment.py
+++ Amg_evaluation_experiment.py
@@ -1,19 +1,25 @@
 import pandas as pd
 import glob
 import itertools
+import torch
 import numpy as np
+import torchvision.ops.boxes as bops
 
 from segment_anything.automatic_mask_generator import SamAutomaticMaskGenerator
 from sam2.automatic_mask_generator import SAM2AutomaticMaskGenerator
+from tqdm import tqdm
 
 from Backbone_evaluation_experiment import TestImages, SamTestInference, SamFlavors
 
 
 class AmgSamTestInference(SamTestInference):
 
-    def __init__(self, sam_gen, weights_path, config, amg_config, device="cuda"):
+    def __init__(
+        self, sam_gen, weights_path, config, amg_config, device="cuda", iou_thresh=0.5
+    ):
         super().__init__(sam_gen, weights_path, config, device)
 
+        self.iou_thresh = iou_thresh
         amg_config["model"] = self.sam_model
         if sam_gen == 1:
             self.amg_predictor = SamAutomaticMaskGenerator(**amg_config)
@@ -22,21 +28,78 @@ class AmgSamTestInference(SamTestInference):
         else:
             raise ValueError("Invalid SAM generation")
 
-    def get_sam_inference(self, test_img_objs: list[TestImages]):
+    def get_sam_inference(self, test_img_obj: TestImages):
         gt_masks = []
         pred_masks = []
-        # TODO: adapt to incoming data and postprocess to reuse metrics of parent
-        for test_img_obj in test_img_objs:
-            self.sam_predictor.set_image(test_img_obj.rgb)
-            self.sam_predictor
-            masks = self.amg_predictor.generate(test_img_obj.rgb)
-            pred_mask = masks[0].astype(np.uint8) * 255
-            pred_masks.append(pred_mask)
-            gt_masks.append(test_img_obj.gt_mask)
-        # check if metric computation needs an update
-        metrics = self.compute_metrics(pred_masks, gt_masks)
-        print("N masks", len(pred_masks), len(gt_masks))
+        self.sam_predictor.set_image(test_img_obj.rgb)
+        self.sam_predictor
+        masks = self.amg_predictor.generate(test_img_obj.rgb)
+        pred_masks = [m["segmentation"].astype(np.uint8) * 255 for m in masks]
+        gt_masks = test_img_obj.masks
+        metrics = self.match_masks_and_evaluate(
+            gt_masks=gt_masks, pred_masks=pred_masks
+        )
         return metrics
+
+    def match_masks_and_evaluate(
+        self, gt_masks: list[np.ndarray], pred_masks: list[np.ndarray]
+    ):
+        # TODO: I think there is a faster torchvision implementation for this
+        gt_boxes, gt_masks = self.get_torch_bbox_of_masks(masks=gt_masks)
+        pred_boxes, pred_masks = self.get_torch_bbox_of_masks(masks=pred_masks)
+
+        bbox_iou = bops.box_iou(pred_boxes, gt_boxes)
+
+        total_positives = gt_boxes.shape[0]
+        total_predictions = pred_boxes.shape[0]
+
+        best_pred_matches_for_gt_boxes = torch.max(bbox_iou, dim=0)
+
+        gt_ious_with_pred = self.calculate_ious(
+            pred_masks=pred_masks,
+            gt_masks=gt_masks,
+            gt_idcs=best_pred_matches_for_gt_boxes.indices,
+        )
+        tp = gt_ious_with_pred[gt_ious_with_pred > self.iou_thresh].shape[0]
+        fn = total_positives - tp
+        fp = total_predictions - tp
+
+        recall = tp / (tp + fn)
+        precision = tp / (tp + fp)
+        metrics = {}
+        metrics["recall"] = recall
+        metrics["precision"] = precision
+        metrics["predictions"] = len(pred_masks)
+        metrics["ground_truth"] = len(gt_masks)
+        return metrics
+
+    def calculate_ious(
+        self,
+        pred_masks: list[torch.Tensor],
+        gt_masks: list[torch.Tensor],
+        gt_idcs: torch.Tensor,
+    ):
+        intersections = gt_masks.logical_and(pred_masks[gt_idcs])
+        intersections_sum = intersections.sum((1, 2))
+        unions = gt_masks.logical_or(pred_masks[gt_idcs])
+        unions_sum = unions.sum((1, 2))
+        ious = intersections_sum / unions_sum
+        return ious
+
+    def get_torch_bbox_of_masks(self, masks: list[np.ndarray]):
+        boxes = []
+        mask_color = 255
+        for mask in masks:
+            y_idcs, x_idcs = np.where(
+                np.all(mask.reshape(*mask.shape, 1) == mask_color, axis=-1)
+            )
+            bbox_x_min = int(x_idcs.min())
+            bbox_x_max = int(x_idcs.max())
+            bbox_y_min = int(y_idcs.min())
+            bbox_y_max = int(y_idcs.max())
+            boxes.append([bbox_x_min, bbox_y_min, bbox_x_max, bbox_y_max])
+
+        return (torch.tensor(boxes), torch.tensor(np.array(masks)))
 
 
 def create_sam_amg_configs() -> list[dict]:
@@ -57,28 +120,34 @@ def create_sam_amg_configs() -> list[dict]:
 
 
 def main():
-    datasets = ["BackboneExperimentData/MaizeUAV/"]
+    datasets = ["Exp32"]
+    iou_thresh = 0.5
     metric_results = []
     sam_flavors = SamFlavors()
     sam_gen, weights_path, config = next(iter(sam_flavors))
     sam_amg_configs = create_sam_amg_configs()
-    for amg_config in sam_amg_configs:
-        sam = AmgSamTestInference(sam_gen, weights_path, config, amg_config)
-        for dataset in datasets:
-            test_imgs = []
 
-            for test_img_dir in glob.glob(dataset + "/*"):
-                test_img = TestImages(test_img_dir)
-                test_imgs.append(test_img)
+    for dataset in datasets:
 
-            metrics = sam.get_sam_inference(test_imgs)
-            metrics["Dataset"] = dataset
-            print(metrics)
-            metric_results.append(metrics)
-        del sam
+        for test_img_dir in glob.glob(dataset + "/*"):
+            test_img = TestImages(test_img_dir)
 
-    df = pd.DataFrame(metric_results)
-    df.to_csv("MaizeUAV_results.csv", index=False)
+            for amg_config in tqdm(sam_amg_configs):
+                sam = AmgSamTestInference(
+                    sam_gen, weights_path, config, amg_config, iou_thresh=iou_thresh
+                )
+
+                metrics = sam.get_sam_inference(test_img)
+                metrics["Dataset"] = dataset
+                metrics["img_dir"] = test_img_dir
+                amg_config["model"] = "sam_model"
+                metrics.update(amg_config)
+                print(metrics)
+                metric_results.append(metrics)
+                del sam
+
+                df = pd.DataFrame(metric_results)
+                df.to_csv("test.csv", index=False)
 
 
 if __name__ == "__main__":
